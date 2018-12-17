6f57dec59961894ea5331cc6434fa79c46e10041
Poojaaaa <Pooja.gajera11@gmail.com>
2018-12-16 16:08:45 -0800

    including redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 import Welcome from "./components/Welcome";
 import Signup from "./components/auth/Signup";
 import App from "./components/App";
+import reducers from "./reducers";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App>
-      <Route path="/" exact component={Welcome} />
-      <Route path="/signup" component={Signup} />
-    </App>
-  </BrowserRouter>,
+  <Provider store={createStore(reducers, {})}>
+    <BrowserRouter>
+      <App>
+        <Route path="/" exact component={Welcome} />
+        <Route path="/signup" component={Signup} />
+      </App>
+    </BrowserRouter>
+  </Provider>,
   document.querySelector("#root")
 );
